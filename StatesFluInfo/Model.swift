82b765ff6d02b5650a2ec24441fdb31f2cf49ec3
Brandon Maynard <brandontyler@me.com>
2022-05-04 19:42:46 -0700

    Searchable Updates/Model Unique ID
    - Since the flu vaccine info data is fetched from the network initially, you can use the data you already have to filter the list as the user types in the search bar; added filteredStates variable to handle this
    - Noticed the console showing a warning that said “the id occurs multiple times within the collection, this will give undefined results!” so I added a unique identifer to the FluVaccineInfo model object so the List could differentiate between objects with the same strings
    - Updated the validateResponse function to one line; one line functions don’t require the ‘return’ keyword (it is implicit)
    - Commented out unused code with this change
    - Added comments in the project to explain each change

--- StatesFluInfo/Model.swift
+++ StatesFluInfo/Model.swift
@@ -8,8 +8,21 @@
 import Foundation
 
 struct FluVaccineInfo: Codable, Hashable {
+    // In order to resolve the console issue that says:
+    // "the id occurs multiple times within the collection,
+    // this will give undefined results!", you can add a unique
+    // identifier for each model object
+    var id = UUID()
+    
     var geography: String
     var coverage_estimate: String
     var year_season: String
     var dimension: String
+    
+    // However, this will be required now so Codable knows that the id variable
+    // won't be in the json that you download from the network
+    // (CodingKeys will tell Codable to only encode/decode these specific variables)
+    enum CodingKeys: String, CodingKey {
+        case geography, coverage_estimate, year_season, dimension
+    }
 }
